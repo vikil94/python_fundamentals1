25b1dd03b965d6f3050c2129471f04cfdc4fbb8d
Vikil Naik <vikilnaik37@gmail.com>
2019-02-11 14:34:28 -0500

    user input with a while loop and if statements that continue to add to a counter

--- excerise5.py
+++ excerise5.py
@@ -1,16 +1,25 @@
+
+
+
 print("Would you like to run or walk?")
 user_input = input()
 counter = 0
 
+while (user_input == "walk" or user_input == "run"):
+    if(user_input == "walk"):
+        counter += 1
+        print("Distance from home is {}km".format(counter))
+        print("Would you like to run or walk?")
+        user_input = input()
+    elif(user_input == "run"):
+        counter += 5
+        print("Distance from home is {}km".format(counter))
+        print("Would you like to run or walk?")
+        user_input = input()
 
-while user_input == "walk":
-    counter += 1
-    print("Distance from home is {}km".format(counter))
-    print("Would you like to run or walk?")
-    user_input = input()
 
-while user_input == "run":
-    counter += 5
-    print("Distance from home is {}km".format(counter))
-    print("Would you like to run or walk?")
-    user_input = input()
+# while user_input == "run":
+#     counter += 5
+#     print("Distance from home is {}km".format(counter))
+#     print("Would you like to run or walk?")
+#
