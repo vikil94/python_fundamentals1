cb90a027c5503f1e0d122f4a8159d17fd9780757
Vikil Naik <vikilnaik37@gmail.com>
2019-02-11 14:56:28 -0500

    Added some comments to explain how the while loop is working

--- excerise5.py
+++ excerise5.py
@@ -5,6 +5,10 @@ print("Would you like to run or walk?")
 user_input = input()
 counter = 0
 
+
+# The if statements in the while loop allow the user to continue to put in their user_input
+# and also allows the counter to keep running 
+
 while (user_input == "walk" or user_input == "run"):
     if(user_input == "walk"):
         counter += 1
@@ -16,10 +20,3 @@ while (user_input == "walk" or user_input == "run"):
         print("Distance from home is {}km".format(counter))
         print("Would you like to run or walk?")
         user_input = input()
-
-
-# while user_input == "run":
-#     counter += 5
-#     print("Distance from home is {}km".format(counter))
-#     print("Would you like to run or walk?")
-#
