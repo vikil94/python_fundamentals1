42d5277418b98953327d8e5e2ec2591e56890382
Vikil Naik <vikilnaik37@gmail.com>
2019-02-11 12:50:49 -0500

    explored users input as well as parsing values in exercise 3

--- exercise3.py
+++ exercise3.py
@@ -1,2 +1,32 @@
+#
+# print("What is your name")
+# user_name = input()
+# print("Hello {}".format(user_name))
+#
+#
+# secret_password = "please"
+# print("What is the password")
+# password_attempt = input()
+#
+# # this is a boolean if the password is corrent it will return true
+# # if the password is wrong it will return false
+# correct_or_not = (password_attempt == secret_password)
+#
+# print("That's {}!".format(correct_or_not))
+#
+print("How many cookies have been eaten?")
+eaten = input()
 
-print("What is your name")
+# convert user input to integer and subtract it from 12
+remaining_cookies = 12 - int(eaten)
+
+print("There are {} cookies left.".format(remaining_cookies))
+
+# ask the user how old they are and have the program output what year they were born
+
+print("How old are you?")
+user_age = input()
+
+year_born = 2019 - int(user_age)
+
+print("You were born in the year {}".format(year_born))
